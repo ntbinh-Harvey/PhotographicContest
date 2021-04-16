da4a6585c8920ca315dd235df023e393e1267b6c
ntbinh6 <ntbinh6@cmc.com.vn>
2021-04-17 00:23:16 +0700

    update

--- index.js
+++ index.js
@@ -5,20 +5,24 @@
 //   });
 // });
 // Đồng hồ đếm ngược
-$(window).ready(function () {
-  var t = $("#timer"),
-    n = t.data("to-start");
-  function e(n) {
-    t.find(".seconds .value").text(a(n % 60)),
-      t.find(".minutes .value").text(a(Math.trunc(n / 60) % 60)),
-      t.find(".hours .value").text(a(Math.trunc(n / 60 / 60) % 24)),
-      t.find(".days .value").text(a(Math.trunc(n / 60 / 60 / 24)));
-  }
-  function a(t) {
-    return t <= 0 ? "00" : (t = t.toString()).length <= 1 ? "0" + t : t;
-  }
-  e(n),
-    window.setInterval(function () {
-      e((n -= 1));
-    }, 1e3);
-});
+var countDownDate = new Date("Apr 25,2021 12:30:00").getTime();
+  var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    $("#days-value").html(days);
+    $("#minutes-value").html(minutes);
+    $("#hours-value").html(hours);
+    $("#seconds-value").html(seconds);
+    // if (distance < 0) {
+    //   clearInterval(x);
+    //   $(".join").html("hết hạn");
+    //   $(".countdown").addClass("count");
+    //   $(".join").addClass("expire").removeClass("join");
+    // }
+  }, 1000);
